fb3e3ff109436f5f3e560520b575c203a09aaa30
Hans Reichgelt <hreichgelt06@gmail.com>
2022-06-23 09:59:36 -0400

    adding requirements for each specific file

---
+++ db/query.sql
@@ -0,0 +1,3 @@
+/* file needs the following:
+select *
+FROM IE - course_name */
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+/* file needs the following
+ability to prepopule database,
+functions for performing sql queries,
+constructor function or class for organizing  */
